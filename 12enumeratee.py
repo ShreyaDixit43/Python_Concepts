fd3df4283d416a659bfc0d6ccde503834493a4ea
ShreyaDixit43 <161469650+ShreyaDixit43@users.noreply.github.com>
2025-06-27 21:48:42 +0530

    Update 12enumeratee.py

--- 12enumeratee.py
+++ 12enumeratee.py
@@ -1,22 +1,22 @@
 marks = [12, 23, 45, 98, 45, 1, 47]
 
-# index = 0
-# for mark in marks:
-#     print(mark)
-#     if(index == 3):
-#         print("Harry, awesome!")
-#     index += 1
+index = 0
+for mark in marks:
+    print(mark)
+    if(index == 3):
+        print("Harry, awesome!")
+    index += 1
 
 #use of enumerate
-# for index, mark in enumerate (marks):
-#     print(mark)
-#     if(index == 3):
-#         print("Harry, awesome!")
+for index, mark in enumerate (marks):
+    print(mark)
+    if(index == 3):
+        print("Harry, awesome!")
 
 #using enumerate in list
-# fruits = ['apple', 'banana', "mango"]
-# for index, fruit in enumerate(fruits):
-#     print(index, fruits)
+fruits = ['apple', 'banana', "mango"]
+for index, fruit in enumerate(fruits):
+    print(index, fruits)
 
 #changing the start index
 fruits = ['apple', 'banana', "mango"]
